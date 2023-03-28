57cc3d50e005f2cb9768d2235e78ea6e6ef181a9
dexterIshere <99083525+dexterIshere@users.noreply.github.com>
2023-03-28 13:29:12 +0200

    type saf

--- src/pages/api/verifyRecaptcha.ts
+++ src/pages/api/verifyRecaptcha.ts
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import fetch from "node-fetch";
 
+interface RecaptchaResponse {
+    success: boolean;
+    score?: number;
+    challenge_ts?: string;
+    hostname?: string;
+    'error-codes'?: string[];
+}
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
@@ -12,9 +20,9 @@ export default async function handler(
         { method: "POST" }
     );
 
-    const data = await response.json();
+    const data = await response.json() as RecaptchaResponse;
 
-    if (data.success && data.score >= 0.5) {
+    if (data.success && data.score && data.score >= 0.5) {
         res.status(200).json({ success: true, score: data.score });
     } else {
         res.status(400).json({ success: false });
