7b3072c8f2bfe10d5aa514360fb1cac08496ab8e
dexterIshere <99083525+dexterIshere@users.noreply.github.com>
2023-04-22 01:27:49 +0200

    vidéo

--- src/pages/components/TeoTheCoach.tsx
+++ src/pages/components/TeoTheCoach.tsx
@@ -29,7 +29,22 @@ const TeoTheCoach: React.FC = () => {
     <div className={style.CoatchC}>
       <div className={style.vidC}>
         <div className={style.Digitbg}></div>
-        <div className={style.vid}></div>
+        <div className={style.vid}>
+          <iframe
+            src="https://player.vimeo.com/video/820000985?h=682e4b91e7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+            title="TateCoach"
+          ></iframe>
+        </div>
       </div>
       <div className={style.ExplainC}>
         <div className={style.DigitTitle}>
